da6860dbe95411337e980afe08361a236bc32d5e
hamidur800 <hamidur800t@gmail.com>
2025-02-15 10:17:47 +0600

    therb commit

--- calculato.js
+++ calculato.js
@@ -1,8 +1,8 @@
-let YourName = prompt("Your Name ?");
-let Partnername = prompt("Your Partnername")
+// let YourName = prompt("Your Name ?");
+// let Partnername = prompt("Your Partnername")
 
-let number = Math.random() * 100;
-number = Math.floor(number) + 1;
+// let number = Math.random() * 100;
+// number = Math.floor(number) + 1;
 
 
-alert(YourName + " and " + Partnername + "'s Love Percentage is:" + number + "%");
+// alert(YourName + " and " + Partnername + "'s Love Percentage is:" + number + "%");
