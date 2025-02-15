07b719641924ed082548042981dfcc527a6bda28
hamidur800 <hamidur800t@gmail.com>
2025-02-15 09:55:45 +0600

    first commit

---
+++ calculato.js
@@ -0,0 +1,8 @@
+let YourName = prompt("Your Name ?");
+let Partnername = prompt("Your Partnername")
+
+let number = Math.random() * 100;
+number = Math.floor(number) + 1;
+
+
+alert(YourName + " and " + Partnername + "'s Love Percentage is:" + number + "%");
